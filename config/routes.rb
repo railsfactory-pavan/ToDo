5abcf46d3be93f0e808ea52f739a33359c7b927f
railsfactory-pavan <pavanrf07@gmail.com>
2019-02-23 06:54:00 +0530

    api routing done

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+
   root :to => "to_do_lists#index"
   resources :to_do_lists
 
+  # get '/api/v1/to_do_lists', to: 'api/v1/to_do_lists#index'
+  # post '/api/v1/to_do_lists', to: 'api/v1/to_do_lists#create'
+  # get '/api/v1/to_do_lists/:id', to: 'api/v1/to_do_lists#show'
+  # patch '/api/v1/to_do_lists/:id', to: 'api/v1/to_do_lists#update'
+  # put '/api/v1/to_do_lists/:id', to: 'api/v1/to_do_lists#update'
+  # delete '/api/v1/to_do_lists/:id', to: 'api/v1/to_do_lists#destroy'
+
   namespace :api do
-    namespace :v1 do
-      resources :to_do_lists
+    namespace :v1, defaults: { format: :json } do
+
+      resources :to_do_lists, only: [:index, :create, :show, :update, :destroy]
     end
   end
 end
